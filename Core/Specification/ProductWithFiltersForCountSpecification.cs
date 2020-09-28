e84cbbb1f7b40f0b57c733939966cc3ab8a9f47d
Mahmoud Elmaghraby <mahmoud.elmaghraby2018@gmail.com>
2020-09-29 01:51:31 +0200

    Implimiting Search method

--- Core/Specification/ProductWithFiltersForCountSpecification.cs
+++ Core/Specification/ProductWithFiltersForCountSpecification.cs
@@ -6,11 +6,11 @@ namespace Core.Specification
     {
         public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
         : base(x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search))&&
                 (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
                 (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
         {
-            
         }
     }
 }
