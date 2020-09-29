d9867e9e3ccd8c85ae357c174cd4e7a9adc6f9ba
Mahmoud Elmaghraby <mahmoud.elmaghraby2018@gmail.com>
2020-09-29 17:08:54 +0200

    add nav-bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Market';
+  product: any[];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost5001/api/products').subscribe((response: any) => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
