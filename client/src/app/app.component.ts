3acf4771393878b50fc99d30a47766c5dcfeee18
Mahmoud Elmaghraby <mahmoud.elmaghraby2018@gmail.com>
2020-09-29 19:16:35 +0200

    g some components and orgnize folders

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
+import { IProduct } from './shared/models/product';
+import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
