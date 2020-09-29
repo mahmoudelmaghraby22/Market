e463296bea061c17c27ccc82a12f3476bb3bc872
Mahmoud Elmaghraby <mahmoud.elmaghraby2018@gmail.com>
2020-09-29 22:56:43 +0200

    seeding data to angular, desgin shop page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { IProduct } from './shared/models/product';
-import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +8,9 @@ import { IPagination } from './shared/models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Market';
-  products: IProduct[];
 
-  constructor(private http: HttpClient){}
+  constructor(){}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
   }
 }
