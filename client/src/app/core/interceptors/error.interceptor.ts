c766fcf3298e6352b662b77a69f8e7b2dc6123f5
Mahmoud Elmaghraby <mahmoud.elmaghraby2018@gmail.com>
2020-09-30 17:39:19 +0200

    improve server error

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 
@@ -28,7 +28,8 @@ export class ErrorInterceptor implements HttpInterceptor {
                         this.router.navigateByUrl('/not-found');
                     }
                     if (error.status === 500) {
-                        this.router.navigateByUrl('/server-error');
+                        const navigationExtras: NavigationExtras = {state: {error: error.error}};
+                        this.router.navigateByUrl('/server-error', navigationExtras);
                     }
                 }
                 return throwError(error);
